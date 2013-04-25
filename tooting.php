ef59d6a8c4ed48f5e8917d646fe937206f6f80ee
Giuseppe Sollazzo <puntofisso@gmail.com>
2013-04-25 16:22:20 +0100

    Python working, some progress on php

--- tooting.php
+++ tooting.php
@@ -20,47 +20,52 @@ class MyStream {
         $this->buffer = $lines[$nb_lines-1];
         unset($lines[$nb_lines-1]);
 
+
         // Here, do your work with the lines you have in the buffer
         //var_dump($lines);
 	foreach ($lines as $line) {
-		$dataarray=explode(",", $line);
-		$StopPointName=$dataarray[1];
-		$StopID=$dataarray[2];
-		$StopCode1=$dataarray[3];
-		$StopCode2=$dataarray[4];
-		$StopPointState=$dataarray[5];
-		$StopPointType=$dataarray[6];
-		$StopPointIndicator=$dataarray[7];
-		$Towards=$dataarray[8];
-		$Bearing=$dataarray[9];
-		$Latitude=$dataarray[10];
-		$Longitude=$dataarray[11];
-		$VisitNumber=$dataarray[12];
-		$TripID=$dataarray[13];
-		$VehicleID=$dataarray[14];
-		$RegistrationNumber=$dataarray[15];
-		$LineID=$dataarray[16];
-		$LineName=$dataarray[17];
-		$DirectionID=$dataarray[18];
-		$DestinationText=$dataarray[19];
-		$DestinationName=$dataarray[20];
-		$EstimatedTime=$dataarray[21];
-		$ExpireTime=$dataarray[22];
+		echo $line;
+		//$dataarray=explode(",", $line);
+		//$StopPointName=$dataarray[1];
+		//$StopID=$dataarray[2];
+		//$StopCode1=$dataarray[3];
+		//$StopCode2=$dataarray[4];
+		//$StopPointState=$dataarray[5];
+		//$StopPointType=$dataarray[6];
+		//$StopPointIndicator=$dataarray[7];
+		//$Towards=$dataarray[8];
+		//$Bearing=$dataarray[9];
+		//$Latitude=$dataarray[10];
+		//$Longitude=$dataarray[11];
+		//$VisitNumber=$dataarray[12];
+		//$TripID=$dataarray[13];
+		//$VehicleID=$dataarray[14];
+		//$RegistrationNumber=$dataarray[15];
+		//$LineID=$dataarray[16];
+		//$LineName=$dataarray[17];
+		//$DirectionID=$dataarray[18];
+		//$DestinationText=$dataarray[19];
+		//$DestinationName=$dataarray[20];
+		//$EstimatedTime=$dataarray[21];
+		//$ExpireTime=$dataarray[22];
 		// now work on data array
 	}
         return strlen($data);
     }
 }
 
-stream_wrapper_register("test", "MyStream")
-    or die("Failed to register protocol");
 
-$ReturnList='StopPointName,StopID,StopCode1,StopCode2,StopPointState,StopPointType,StopPointIndicator,Towards,Bearing,Latitude,Longitude,VisitNumber,TripID,VehicleID,RegistrationNumber,LineID,LineName,DirectionID,DestinationText,DestinationName,EstimatedTime,MessageUUID,MessageText,MessageType,MessagePriority,ExpireTime,BaseVersion';
+//$ReturnList='StopPointName,StopID,StopCode1,StopCode2,StopPointState,StopPointType,StopPointIndicator,Towards,Bearing,Latitude,Longitude,VisitNumber,TripID,VehicleID,RegistrationNumber,LineID,LineName,DirectionID,DestinationText,DestinationName,EstimatedTime,MessageUUID,MessageText,MessageType,MessagePriority,ExpireTime,BaseVersion';
+//$ReturnList='StopPointName,StopID,StopCode1,StopCode2,LineName,DirectionID,DestinationText,EstimatedTime,MessageUUID';
 
-$Stops='77501';
+//$Stops='77501,77502,77503';
 
-$json_url = 'http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1?StopCode1='.$Stops.'&ReturnList='.$ReturnList; 
+//$json_url = 'http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1?StopCode1='.$Stops.'&ReturnList='.$ReturnList; 
+stream_wrapper_register("test", "MyStream") or die("Failed to register protocol");
 
+$json_url = 'http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1?StopCode1=77501,77502,77503&ReturnList=StopPointName,StopID,StopCode1,StopCode2,StopPointState,StopPointType,StopPointIndicator,Towards,Bearing,Latitude,Longitude,VisitNumber,TripID,VehicleID,RegistrationNumber,LineID,LineName,DirectionID,DestinationText,DestinationName,EstimatedTime,MessageUUID,MessageText,MessageType,MessagePriority,ExpireTime,BaseVersion';
+
+echo $json_url;
 
 include('config.php');
 // Open the "file"
@@ -73,7 +78,7 @@ curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_DIGEST);
 curl_setopt($ch, CURLOPT_USERPWD, $username.":".$password);
 curl_setopt($ch, CURLOPT_READFUNCTION, 'read_callback');
 curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_setopt($ch, CURLOPT_BUFFERSIZE, 256);
+curl_setopt($ch, CURLOPT_BUFFERSIZE, 10);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_FILE, $fp);    
 curl_exec($ch); 
---
+++ tooting.py
@@ -0,0 +1,54 @@
+import pycurl, simplejson, datetime
+from config import USER, PASS
+BASE_URI = 'http://countdown.api.tfl.gov.uk/interfaces/ura/stream_V1'
+QUERY_SECTION = '?StopCode1=77501,77502,77503&ReturnList=Stoppointname,VehicleID,RegistrationNumber,LineName,DestinationName,EstimatedTime,ExpireTime,latitude,longitude' 
+
+
+
+class Client:
+  def __init__(self):
+  	
+  	auth = (USER, PASS)
+
+	self.buffer = ""
+
+	self.conn = pycurl.Curl()
+	self.conn.setopt(pycurl.HTTPAUTH, pycurl.HTTPAUTH_DIGEST)
+	self.conn.setopt(pycurl.USERPWD, "%s:%s" % auth )
+	#self.conn.setopt(pycurl.HTTPHEADER, ['Accept: application/json'])
+	self.conn.setopt(pycurl.URL, BASE_URI + QUERY_SECTION )
+	self.conn.setopt(pycurl.WRITEFUNCTION, self.on_receive)
+	self.conn.perform()
+
+	
+  def on_receive(self, data):
+
+	print data
+	#self.buffer += data
+	## Handle complete line
+	#if data.endswith("]") and self.buffer.strip():
+		
+		
+	#	#t = self.buffer.split("\r\n")
+	#	#for i in t:
+	#	#	try:
+	#	#		row = simplejson.loads(i)
+	#	#		if len(row) > 6:
+	#	#			row[8] = datetime.datetime.fromtimestamp( row[8] / 1000 )
+	#	#		print row
+	#	#	except simplejson.decoder.JSONDecodeError, e:
+	#	#		print e
+	#	#	except TypeError, e:
+	#	#		print 'Type Error', e
+	#	print self.buffer
+	#	self.buffer = ""
+		
+		#print 'Complete'
+
+
+def main():
+	client = Client()
+
+if __name__ == '__main__':
+	main()
+
